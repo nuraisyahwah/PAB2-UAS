c58d8d53f74fad277b784f5d8027aa2d0cbb54c6
dindaputriay <dindaputri@mhs.mdp.ac.id>
2023-06-08 10:32:53 +0700

    update
    Membuat file APIService, ValueNoData

---
+++ app/src/main/java/com/example/absensi/APIService.java
@@ -0,0 +1,4 @@
+package com.example.absensi;
+
+public interface APIService {
+}
---
+++ app/src/main/java/com/example/absensi/ValueNoData.java
@@ -0,0 +1,20 @@
+package com.example.absensi;
+
+
+import com.google.gson.annotations.SerializedName;
+
+public class ValueNoData {
+    @SerializedName("success")
+    private int success;
+
+    @SerializedName("message")
+    private String message;
+
+    public int getSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
